9204d1a3114972238a91b321bf51cb51ed4b3e2d
hubert <kiszkahub@gmail.com>
2026-07-04 19:47:09 +0200

    Repaired

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_container_registry_task" "build" {
     os = "Linux"
   }
   docker_step {
-    dockerfile_path      = "application/Dockerfile"
+    dockerfile_path      = "Dockerfile"
     context_path         = var.git_repo_url
     context_access_token = var.git_pat
     image_names          = ["${var.image_name}:latest"]
--- task08/terraform.tfvars
+++ task08/terraform.tfvars
@@ -1,6 +1,6 @@
 name_prefix  = "cmtr-7c5kvppd-mod8"
 location     = "West Europe"
-git_repo_url = "https://github.com/kiszkahubert/ep-tf.git#master:task08"
+git_repo_url = "https://github.com/kiszkahubert/ep-tf.git#master:task08/application"
 tags = {
   Creator = "hubert_kiszka@epam.com"
 }
